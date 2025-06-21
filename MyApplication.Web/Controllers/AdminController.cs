6b033c60f62c5fe155779ae50fdb3e45d3156852
Faydin25 <ferhat_yigit_aydin@hotmail.com>
2025-06-21 14:26:55 +0000

    EDİTED

--- MyApplication.Web/Controllers/AdminController.cs
+++ MyApplication.Web/Controllers/AdminController.cs
@@ -234,5 +234,26 @@ namespace MyApplication.Web.Controllers
 
             return RedirectToAction("Users");
         }
+
+        // GET: /Admin/DeleteUser/5
+        public async Task<IActionResult> DeleteUser(int? id)
+        {
+            if (id == null) return NotFound();
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null) return NotFound();
+            return View(user);
+        }
+
+        // POST: /Admin/DeleteUser/5
+        [HttpPost, ActionName("DeleteUserConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUserConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Users));
+        }
     }
 } 
